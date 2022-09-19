7d20e19586506b0b90f80c10901ef5d37054e70a
Isin Altinkaya <isinaltinkaya@gmail.com>
2022-09-19 09:02:19 +0200

    edit functions

--- functions.R
+++ functions.R
@@ -1,4 +1,16 @@
 
+################################################################################
+# LOAD LIBRARIES
+
+library(microbenchmark)
+library(splines)
+library(compiler)
+library(readr)
+library(tidyr)
+
+
+################################################################################
+
 ###
 
 # Computing the vector of knot differences (b - a) 
@@ -6,7 +18,7 @@
 ## we try to use a method that is alternative to diff
 ## and bytecompiled
 
-library(compiler)
+
 
 diff_v2 <- function(v) {
   v[2:length(v)]-v[1:(length(v)-1L)]
@@ -50,41 +62,31 @@ get_diff <- compiler::cmpfun(function(v) {
 
 ## 
 
-################################################################################
-# LOAD LIBRARIES
-
-library(microbenchmark)
-library(splines)
-
-data(NuukData)
-data("Nuuk_year")
-
 
 ################################################################################
 # LOAD DATA
 
 
-CSwR_dir<-"../CSwR"
+CSwR_dir<-"../CSwR/"
 
 
-Nuuk <- read_table(paste0(CSwR_dir,"/data/nuuk.dat.txt"), 
-                   col_names = c("Year", 1:12), 
-                   na = "-999", 
-                   skip = 1) %>% 
-  gather(key = "Month", value = "Temp_Nuuk", -Year, convert = TRUE) %>% 
-  mutate(Temp_Nuuk = Temp_Nuuk / 10) %>%
-  filter("Year">1866)
+nuuk_dat <- read_table(paste0(CSwR_dir,"data/nuuk.dat.txt"), 
+                        col_names = c("year", 1:12), na = "-999", skip = 1) %>% 
+  gather(key = "Month", value = "Temperature", -year, convert = TRUE) %>% 
+  mutate(Temperature = Temperature / 10) %>% drop_na()
 
-Qaqortoq <- read_table(paste0(CSwR_dir,"/data/qaqortoq.dat"),
-                       col_names = c("Year", 1:12), 
-                       na = "-999", 
-                       skip = 1) %>% 
-  gather(key = "Month", value = "Temp_Qaqortoq", -Year, convert = TRUE) %>% 
-  mutate(Temp_Qaqortoq = Temp_Qaqortoq / 10) %>% 
-  filter("Year" > 1866)
+Nuuk_year <- group_by(nuuk_dat, year) %>% 
+  summarise(mean = mean(Temperature,na.rm = TRUE),
+            median = median(Temperature,na.rm = TRUE),
+            high = max(Temperature,na.rm = TRUE), 
+            low = min(Temperature,na.rm = TRUE))
 
-p_Nuuk <- ggplot(Nuuk, aes(x = Year, y = Temperature)) + 
-  geom_point()
+
+ggplot(Nuuk_year, aes(x = year, y = mean)) + 
+  geom_point() + geom_smooth(se = FALSE) + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, 10), color = "red", se = FALSE) + 
+  geom_smooth(method = "gam", formula = y ~ s(x, bs = "cr"), color = "purple", se = FALSE)
+
 
 ################################################################################
 
@@ -173,7 +175,8 @@ penalty_matrix<-function(inner_knots){
 smoother_v1 <- function(x,y, lambda){ 
   
   inner_knots<-x
-  knots <- sort(c(rep(range(inner_knots), 3), inner_knots),method="quick")
+  knots <- sort(c(rep(range(inner_knots), 3), inner_knots),
+                method="quick")
   
   Phi <- splineDesign(knots,inner_knots)
   Omega <- penalty_matrix(inner_knots)
@@ -209,7 +212,7 @@ loocv_v1 <- function(lambda, y){
   sum(((y - S %*% y) / (1 - Sii))^2, na.rm = TRUE) 
 }
 
-lambda <- seq(1,200,1)
+#lambda <- seq(1,200,1)
 LOOCV<-sapply(lambda, loocv_v1, y=data$y)
 LOOCV[which.min(LOOCV)]
 
@@ -217,9 +220,9 @@ qplot(lambda,LOOCV)+
   geom_vline(xintercept = LOOCV[which.min(LOOCV)], color="red")
 
 
-get_best_lambda_loocv_v1 <- function(y, n=200){
+get_best_lambda_loocv_v1 <- function(y, n=20){
   lambda <- seq(1,n,1)
-  LOOCV<-sapply(lambda, loocv_v1, y=data$y)
+  LOOCV<-sapply(lambda, loocv_v1, y=y)
   LOOCV[which.min(LOOCV)]
 }
 
@@ -227,16 +230,15 @@ get_best_lambda_loocv_v1 <- function(y, n=200){
 get_best_lambda_loocv_v1(data$y)
 
 
-
-
-smoother_loocv_v1<-function(x,y){
+smoother_loocv_v1<-function(x,y,...){
   inner_knots<-x
-  knots <- sort(c(rep(range(inner_knots), 3), inner_knots),method="quick")
+  knots <- sort(c(rep(range(inner_knots), 3), inner_knots),
+                method="quick")
   
   Phi <- splineDesign(knots,inner_knots)
   Omega <- penalty_matrix(inner_knots)
   
-  lambda<-get_best_lambda_loocv_v1(data$y)
+  lambda<-get_best_lambda_loocv_v1(y,...)
   
   Phi %*% solve(
     crossprod(Phi) + lambda * Omega, 
@@ -246,54 +248,59 @@ smoother_loocv_v1<-function(x,y){
 
 
 
+x<-seq(0,20,0.1)
+y <- sin(x) + rnorm(x)
+
+data<-data.frame(x,y)
+
+ggplot(data, aes(x = x, y = y)) + 
+  geom_point()+
+  geom_line(aes(y=loocv_smoother(x,y,seq(0,1,0.1))),color="blue")
 
 
+wave <- seq(0, 10, 0.2)
+y <- sin(wave) + rnorm(wave)
+data<-data.frame(x=wave,y=y)
+
+#plot(x,y)
+
+#wave <- seq(0, 10, 0.2)
+#y <- sin(wave) + rnorm(wave)
+
+smoother_loocv_v1(data$x,data$y)
 
 
-
-
-
-
-
-
-
-### Nuuk data 
-
-Nuuk <- read_table(paste0(CSwR_dir,"/data/nuuk.dat.txt"), 
-                    col_names = c("Year", paste0("V",c(1:12))), 
-                    na = "-999", 
-                    skip = 1)
-
-
-Nuuk <- read_table(paste0(CSwR_dir,"/data/nuuk.dat.txt"), 
-                   col_names = c("Year",1:12), 
-                   na = "-999", 
-                   skip = 1)
-
-
-data<-Nuuk%>%
-  gather(key = "Month", value = "Temperature", -Year, convert = TRUE) %>% 
-  mutate(Temperature = Temperature / 10)
-
-data<-data[data$Year > 1866,]
-
-  
-data<-group_by(data,Year)%>%
-  summarise(
-    Temperature=mean(Temperature))
-
-data<-data[! is.na(data$Temperature),]
-#data%>%summarise(Temperature=mean(data$Temperature))
-#data <- tibble(x = data$Year, y = mean(data$Temperature))
-
+#data<-data.frame(x=wave,y=y)
 
 ggplot(data, aes(x = x, y = y)) + 
   geom_point()+
   geom_line(aes(y=smoother_loocv_v1(x,y)),color="blue")
 
 
+ggplot(data, aes(x = x, y = y)) + 
+  geom_point()+
+  geom_line(aes(y=loocv_smoother(x,y, seq(0, 1, 0.1))),color="blue")
+
+
+ggplot(data, aes(x = x, y = y)) + 
+  geom_point()+
+  geom_line(aes(y=smoother_loocv_v1(x,y)),color="blue")
+
+ggplot(data, aes(x = x, y = y)) + 
+  geom_point()+
+  geom_line(aes(y=loocv_smoother(x,y,seq(0,1,0.1))),color="blue")
+
+plot(wave, y)
+lines(wave, l_m$S %*% y)
+lines(wave, smoother_loocv_v1(wave,y))
+
 
 ggplot(data, aes(x = Year, y = Temperature)) + 
   geom_point()+
   geom_line(aes(y=smoother_loocv_v1(data$Year,data$Temperature)),color="blue")
 
+ggplot(Nuuk_year, aes(x = year, y = mean)) + 
+  geom_point() + geom_smooth(se = FALSE) + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, 10), color = "red", se = FALSE) + 
+  geom_smooth(method = "gam", formula = y ~ s(x, bs = "cr"), color = "purple", se = FALSE)+
+  geom_line(aes(y=smoother_loocv_v1(Nuuk_year$year,Nuuk_year$mean)),color="blue")
