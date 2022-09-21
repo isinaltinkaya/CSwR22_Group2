4099e26338567eb8d5c48b90ee161db01e61b5fc
IbreathForALiving <sebdumipvl@gmail.com>
2022-09-21 19:20:49 +0200

    added functions

--- functions.R
+++ functions.R
@@ -78,7 +78,7 @@ optimize_lambda_loocv_svd_v1 <- function(y,D,m, md=0.1){
   structure(list(f_hat=best_f_hat, lambda=best_lambda, loss=best_loss))
 }
 
-optimize_lambda_loocv_v1<-function(y,Phi,Omega,m,md=0.1){
+optimize_lambda_loocv_v1<-function(y,Phi,Omega,m,md=0.1){ #same (almost) as loocv_smoother
   
   best_loss<-Inf
   best_lambda<-NULL
@@ -192,5 +192,86 @@ smoother_v2<-function(data, x,y, lambda,order=4, subsample=TRUE, useSVD = FALSE,
     }
   }    
   structure(list(xs=x,ys=f_hat),class="smoother")
+}
+
+loocv_smoother <- function(inner_knots, y, lambda){
+  n <-  length(lambda) #length(x) == length(y)
+  loss <- rep(Inf, n) #store the loss for each lambda
+  Sm <- list() #store all the S_lambda matrices
+  sm_y <- list()
+  
+  Omega <- pen_mat(inner_knots) #penalty matrix
+  #Phi matrix
+  Phi <- splineDesign(c(rep(range(inner_knots), 3), inner_knots), inner_knots)
+  
+  #Phi^T Phi
+  tphi_phi <- crossprod(Phi) #spares computation time: it's same for all lambdas
+  
+  for(i in 1:n){ #iterate over all lambdas
+    if(det(tphi_phi + lambda[i] * Omega) != 0){ #make sure we have a solution
+      #S_lambda matrix
+      S <- Phi %*% solve(tphi_phi + lambda[i] * Omega) %*% t(Phi)
+      
+      #store the matrix into a list
+      Sm[[i]] <- S #spares computation time for the return value
+
+      sm_y[[i]] <- S %*% y
+      
+      #compute and add loss
+      loss[i] <- mean(((y - S * y)/(1 - diag(S)))^2, na.rm = TRUE)
+    }
+  }
+  
+  #index of best lambda i.e smallest loss i.e corresponding S matrix
+  ind <- which.min(loss)
+  
+  #return all the values we're interested in
+  return(list(lambda=lambda, best_lambda=lambda[ind],
+              S=Sm[[ind]], y=sm_y[[ind]], loss=loss, index=ind))
+}
+
+loocv_smoother.v2 <- function(inner_knots, x, y, lambda){
+  n <-  length(lambda) #length(x) == length(y)
+  loss <- rep(Inf, n) #store the loss for each lambda
+  min_loss <- Inf #container for the minimal loss
+  ind <- 1 #index of best lambda i.e smallest loss
+  Sm <- 0 #hold the best S_lambda matrix
+  sm_y <- 0
+  
+  Omega <- pen_mat(inner_knots) #penalty matrix
+  #Phi matrix
+  Phi <- splineDesign(sort(c(rep(range(inner_knots), 3), inner_knots)), x)
+  
+  #Phi^T Phi
+  tphi_phi <- crossprod(Phi) #spares computation time: it's same for all lambdas
+  
+  for(i in 1:n){ #iterate over all lambdas
+    if(det(tphi_phi + lambda[i] * Omega) != 0){ #make sure we have a solution
+      #S_lambda matrix
+      S <- Phi %*% solve(tphi_phi + lambda[i] * Omega) %*% t(Phi)
+      
+      #compute and add loss
+      loss[i] <- mean(((y - S * y)/(1 - diag(S)))^2, na.rm = TRUE)
+      
+      if(loss[i] <= min_loss){
+        min_loss <- loss[i]
+        Sm <- S
+        sm_y <- S %*% y
+        ind <- i
+      }
+    }
+  }
+  
+  #return all the values we're interested in
+  return(list(lambda=lambda, best_lambda=lambda[ind],
+              S=Sm, loss=loss, index=ind, y=sm_y))
+}
+
+read_bike_series <- function(){
+  d <- read.csv("data/bike_sharing_daily.csv", sep=',')
+  d = d[c('dteday', 'cnt')]
+  d$dteday <- as.Date(d$dteday, format =  "%Y-%m-%d")
+  d$ind <- seq(1,length(d$cnt),1)
+  return(d)
 }
 
